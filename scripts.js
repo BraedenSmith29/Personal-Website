9a01c48e08a867d5ef86580fd14d6bfe398406c6
Braeden Smith <braedensmith629@gmail.com>
2023-01-31 21:18:22 -0600

    Basic beginning page

---
+++ scripts.js
@@ -0,0 +1,36 @@
+function hidePages() {
+	document.getElementById("home-page").setAttribute("hidden", "");
+	document.getElementById("portfolio-page").setAttribute("hidden", "");
+	document.getElementById("qualifications-page").setAttribute("hidden", "");
+	document.getElementById("service-page").setAttribute("hidden", "");
+	document.getElementById("ai-page").setAttribute("hidden", "");
+}
+
+function toHomePage() {
+	hidePages()
+	document.getElementById("home-page").removeAttribute("hidden");
+}
+
+function toPortfolioPage() {
+	hidePages()
+	document.getElementById("portfolio-page").removeAttribute("hidden");
+}
+
+function toQualificationsPage() {
+	hidePages()
+	document.getElementById("qualifications-page").removeAttribute("hidden");
+}
+
+function toServicePage() {
+	hidePages()
+	document.getElementById("service-page").removeAttribute("hidden");
+}
+
+function toAiPage() {
+	hidePages()
+	document.getElementById("ai-page").removeAttribute("hidden");
+}
+
+function toggleTheme() {
+	// Do Nothing
+}
